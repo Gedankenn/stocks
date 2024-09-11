1255f5cab61d295b55d185b18828591c72131a26
FabioStella <gedankenn@gmail.com>
2024-09-11 00:57:00 -0300

    update
    improved the library stocks for better usage of its methods and added
    some new functions to the main code

---
+++ broker.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+'''
+This is the main script that will run the broker.
+Author: Fabio Slika Stella
+Date: 2024-09-05
+
+'''
+
+
+import stocks as st
+from bcolors import bcolors
+import sys
+
+
+def menu_listing():
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}Broker Menu{bcolors.ENDC}")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}h{bcolors.ENDC} - Display this help menu")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}a{bcolors.ENDC} - Add a stock to the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}r{bcolors.ENDC} - Remove a stock from the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}l{bcolors.ENDC} - List all stocks in the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}p{bcolors.ENDC} - Plot a stock")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}m{bcolors.ENDC} - Show the main menu")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}q{bcolors.ENDC} - Quit the broker")
+    print(f"{bcolors.ENDC}\n\n\n")
+    
+    key = input(f"{bcolors.OKGREEN}{bcolors.BOLD}Enter your choice: {bcolors.ENDC}")
+    return key
+
+def menu_helper():
+    '''
+        This function will display the help menu
+    '''
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}Broker Help Menu{bcolors.ENDC}")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -h{bcolors.ENDC}                                       - Display this help menu")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -a <stock> <buy value> <buy date>{bcolors.ENDC}        - Add a stock to the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -s <stock> <sell value> <sell date>{bcolors.ENDC}      - Sell a stock from the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -r <stock>{bcolors.ENDC}                               - Remove a stock from the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -l{bcolors.ENDC}                                       - List all stocks in the portfolio")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -p <stock> <start date> <end date>{bcolors.ENDC}       - Plot a stock")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -m{bcolors.ENDC}                                       - Show the main menu")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}broker.py -q{bcolors.ENDC}                                       - Quit the broker")
+
+def menu_navigation(option, stocks):
+    if option == "a":
+        print("Adding a stock")
+    elif option == "r":
+        print("Removing a stock")
+    elif option == "l":
+        print("Listing all owned stocks")
+        for stock in stocks:
+            print(stock)
+
+    elif option == "p":
+        print("Plotting a stock")
+    elif option == "m":
+        print("Showing the main menu")
+    elif option == "q":
+        print("Quitting the broker")
+    else:
+        print("Invalid option")
+        sys.exit(1)
+
+def load_my_stocks():
+    stocks = st.read_tickets()
+    my_stocks = []
+    for i in range(len(stocks["SYMBOL"])):
+        my_stocks.append(st.Stock(stocks["SYMBOL"][i], stocks["QUANTITY"][i], stocks["BUY_PRICE"][i], stocks["BUY_DATE"][i], stocks["SELL_PRICE"][i], stocks["SELL_DATE"][i]))
+    return my_stocks
+
+def menu(args):
+    option = "-q"
+    if len(args) <= 1:
+        menu_helper()
+        option = menu_listing()
+
+    elif len(args) >= 2:
+        option = args[1].strip('-')
+    
+    if args[1] == "-h": 
+        menu_helper()
+        sys.exit(0)
+    
+    else:
+        stocks = load_my_stocks()
+        menu_navigation(option, stocks)
+        sys.exit(0)
+
+
+
+
+def banner():
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}"
+    "   | |__  _ __ ___ | | _____ _ __\n"
+    "   | '_ \\| '__/ _ \\| |/ / _ \\ '__|\n"
+    "   | |_) | | | (_) |   <  __/ |\n"
+    "   |_.__/|_|  \\___/|_|\\_\\___|_|")
+    print(f"{bcolors.OKGREEN}{bcolors.BOLD}   Made by Fabio Slika Stella!{bcolors.ENDC}")
+    print(f"{bcolors.ENDC}\n\n\n")
+
+
+
+def main():
+    banner()
+    args = sys.argv
+    menu(args)
+
+if __name__ == "__main__":
+    main()
--- stocks.py
+++ stocks.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
+'''
+This is the stocks class that will handle all the stocks operations.
+Author: Fabio Slika Stella
+Date: 2024-09-10
+'''
 
 import investpy as inv
 import yfinance as yfi
 from datetime import datetime, date, timedelta
 import asciichartpy as asciiplot
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
+from bcolors import bcolors
 
 class Stock:
-    def __init__(self, symbol, quantity=1, buy_price=0, buy_date="", sell_price='', sell_date=""):
+    def __init__(self, symbol='', quantity=1, buy_price=0, buy_date="", sell_price='', sell_date=""):
         self.symbol = symbol
         self.quantity = quantity
         self.buy_price = buy_price
@@ -36,24 +30,16 @@ class Stock:
         stocks = inv.get_stocks(country = 'brazil')
         return stocks
 
-    def read_tickets(self):
-        f = open("my_stocks/papers.txt","r")
-        my_stocks = {"SYMBOL":[],"QUANTITY":[],"BUY_PRICE":[],"BUY_DATE":[],"SELL_PRICE":[],"SELL_DATE":[]}
-        lines = f.readlines()
-        f.close()
-        for line in lines:
-            l1 = line.split(",")
-            my_stocks["SYMBOL"].append(l1[0].split(":")[1])
-            my_stocks["QUANTITY"].append(l1[1].split(":")[1])
-            my_stocks["BUY_PRICE"].append(l1[2].split(":")[1])
-            my_stocks["BUY_DATE"].append(l1[3].split(":")[1])
-            my_stocks["SELL_PRICE"].append(l1[4].split(":")[1])
-            my_stocks["SELL_DATE"].append(l1[5].split(":")[1])
-        return my_stocks
-
-
     def __str__(self):
-        return f"Stock: {self.symbol}, Quantity: {self.quantity}, Buy Price: {self.buy_price}, Buy Date: {self.buy_date}, Sell Price: {self.sell_price}, Sell Date: {self.sell_date}"
+        text = f"{bcolors.BOLD}{bcolors.HEADER} ------ Stock ------{bcolors.ENDC}\n"
+        text += f"{bcolors.BOLD}{bcolors.OKGREEN}Symbol: {bcolors.ENDC}{self.symbol.upper()}\n"
+        text += f"{bcolors.BOLD}{bcolors.OKGREEN}Quantity: {bcolors.ENDC}{self.quantity}\n"
+        text += f"{bcolors.BOLD}{bcolors.OKGREEN}Buy Price: {bcolors.ENDC}{self.buy_price}\n"
+        text += f"{bcolors.BOLD}{bcolors.OKGREEN}Buy Date: {bcolors.ENDC}{self.buy_date}\n"
+        text += f"{bcolors.BOLD}{bcolors.OKGREEN}Sell Price: {bcolors.ENDC}{self.sell_price}\n"
+        text += f"{bcolors.BOLD}{bcolors.OKGREEN}Sell Date: {bcolors.ENDC}{self.sell_date}\n"
+        text += f"{bcolors.BOLD}{bcolors.HEADER} -------------------{bcolors.ENDC}\n"
+        return text
 
     def plot_stock(self, start_date, end_date):
         ticket = self.symbol + ".sa"
@@ -74,3 +60,29 @@ class Stock:
         print(f"{bcolors.BOLD}Stock: {self.symbol.upper()}{bcolors.ENDC}")
 
         print(asciiplot.plot(hist, config))
+
+
+    def plot_stock_since_buy(self):
+        '''
+        This method will plot the stock since the buy date
+        '''
+        start_date = self.buy_date
+        end_date = date.today()
+        self.plot_stock(start_date, end_date)
+
+
+def read_tickets():
+    f = open("my_stocks/papers.txt","r")
+    my_stocks = {"SYMBOL":[],"QUANTITY":[],"BUY_PRICE":[],"BUY_DATE":[],"SELL_PRICE":[],"SELL_DATE":[]}
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        l1 = line.split(",")
+        my_stocks["SYMBOL"].append(l1[0].split(":")[1])
+        my_stocks["QUANTITY"].append(l1[1].split(":")[1])
+        my_stocks["BUY_PRICE"].append(l1[2].split(":")[1])
+        my_stocks["BUY_DATE"].append(l1[3].split(":")[1])
+        my_stocks["SELL_PRICE"].append(l1[4].split(":")[1])
+        my_stocks["SELL_DATE"].append(l1[5].split(":")[1].strip("\n"))
+    return my_stocks
+
